7b87e6bd56eddb8063a0e1f5800fc439bc1105e0
EJHacks <ee1912136@gmail.com>
2026-03-09 18:06:49 -0400

    Refactor: Remove Home, Works, script.js, and style.css files; update styles for contributions and hero visuals

--- react-portfolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', color: '#aaa', marginTop: '2rem' }}>
-      <small>&copy; 2025 Eric Edwards. All rights reserved.</small>
-    </footer>
-  );
-}
